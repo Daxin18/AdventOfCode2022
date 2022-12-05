45fd64d8af6de4637b01d1eae4928910f5350d51
Kamil <kamilciag123@gmail.com>
2022-12-05 01:53:52 +0100

    Finished puzzle 1

---
+++ Day01/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            SolvePuzzle1();
+        }
+
+        static void SolvePuzzle1()
+        {
+            //have to go one level higher because it somehow is a .NET console app and everything goes into \bin\Debug\net5.0
+            string[] lines = System.IO.File.ReadAllLines(System.IO.Path.GetFullPath(@"..\..\..\") + @"\input.txt");
+            int maxCaloriesCount = 0;
+            int tmp = 0;
+            int x;
+
+            foreach (string line in lines)
+            {
+                if (int.TryParse(line, out x))
+                {
+                    tmp += x;
+                    if (tmp > maxCaloriesCount)
+                        maxCaloriesCount = tmp;
+                }
+                else
+                {
+                    tmp = 0;
+                }
+            }
+
+            Console.WriteLine(maxCaloriesCount);
+        }
+    }
+}
