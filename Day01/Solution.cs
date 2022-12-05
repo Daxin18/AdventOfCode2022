cab0b4ccb3eeec26c373ba34ed2ba9da3474139a
Kamil <kamilciag123@gmail.com>
2022-12-05 02:10:01 +0100

    Finished puzzle 2

--- Day01/Solution.cs
+++ Day01/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day01
@@ -7,12 +8,14 @@ namespace Day01
     {
         static void Main(string[] args)
         {
-            SolvePuzzle1();
+            //SolvePuzzle1();
+            SolvePuzzle2();
         }
 
         static void SolvePuzzle1()
         {
             //have to go one level higher because it somehow is a .NET console app and everything goes into \bin\Debug\net5.0
+            //I may have chosen wrong project file
             string[] lines = System.IO.File.ReadAllLines(System.IO.Path.GetFullPath(@"..\..\..\") + @"\input.txt");
             int maxCaloriesCount = 0;
             int tmp = 0;
@@ -33,6 +36,61 @@ namespace Day01
             }
 
             Console.WriteLine(maxCaloriesCount);
+        }
+
+        static void SolvePuzzle2()
+        {
+            int ElvesToFind = 3;
+
+            string[] lines = System.IO.File.ReadAllLines(System.IO.Path.GetFullPath(@"..\..\..\") + @"\input.txt");
+            List<int> elves = CaloriesOfElves(lines);
+
+            elves = GetNHighest(ElvesToFind, elves);
+
+            int sum = 0;
+            foreach (int elf in elves)
+            {
+                sum += elf;
+            }
+
+            Console.WriteLine(sum);
+        }
+        
+        private static List<int> GetNHighest(int n, List<int> list)
+        {
+            List<int> result = new List<int>();   
+
+            list.Sort();
+            list.Reverse();
+
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(list[i]);
+            }
+
+            return result;
+        }
+
+        private static List<int> CaloriesOfElves(string[] lines)
+        {
+            List<int> elves = new List<int>();
+            int tmp = 0;
+            int x;
+
+            foreach (string line in lines)
+            {
+                if (int.TryParse(line, out x))
+                {
+                    tmp += x;
+                }
+                else
+                {
+                    elves.Add(tmp);
+                    tmp = 0;
+                }
+            }
+
+            return elves;
         }
     }
 }
