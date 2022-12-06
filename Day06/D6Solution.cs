83adf52e3419aa8c25d2ddc909fb3d03ad8bb378
Kamil <kamilciag123@gmail.com>
2022-12-06 16:20:14 +0100

    Finished day6 puzzle 1 and 2

--- Day06/D6Solution.cs
+++ Day06/D6Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day06
 {
@@ -13,23 +14,90 @@ namespace Day06
         static void SolvePuzzle1()
         {
             string[] lines = GetLines();
+            Queue<char> last4 = new Queue<char>();
+            int markerSize = 4;
+            int result = 0;
+
+            //there is only one line, but it doesn't mean I can't make it work with more lines
+            foreach (string line in lines)
+            {
+                foreach(char item in line)
+                {
+                    EnqueueLimitX(ref last4, item, markerSize);
+                    result++;
+
+                    if (MarkerOfSizeXAppeared(last4, markerSize))
+                    {
+                        goto MarkerFound;
+                    }
+                }    
+            }
+
+            MarkerFound:
+            Console.WriteLine(result);
         }
+
+        private static void EnqueueLimitX(ref Queue<char> queue, char item, int limit)
+        {
+            queue.Enqueue(item);
+
+            if(queue.Count > limit)
+            {
+                queue.Dequeue();
+            }
+        }
+        private static bool MarkerOfSizeXAppeared(Queue<char> queue, int size)
+        {
+            bool result = false;
+
+            if (queue.Count == size)
+            {
+                char[] possibleMarker = queue.ToArray();
+                result = true;
+                //an easy solution that hurts my soul, but i have to get it done quickly
+                for(int i = 0; i < size; i++)
+                {
+                    for(int j = i + 1; j < size; j++)
+                    {
+                        if (possibleMarker[i].Equals(possibleMarker[j]))
+                            result = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+
 
         static void SolvePuzzle2()
         {
             string[] lines = GetLines();
+            Queue<char> last4 = new Queue<char>();
+            int markerSize = 14;
+            int result = 0;
+
+            //there is only one line, but it doesn't mean I can't make it work with more lines
+            foreach (string line in lines)
+            {
+                foreach (char item in line)
+                {
+                    EnqueueLimitX(ref last4, item, markerSize);
+                    result++;
+
+                    if (MarkerOfSizeXAppeared(last4, markerSize))
+                    {
+                        goto MarkerFound;
+                    }
+                }
+            }
+
+        MarkerFound:
+            Console.WriteLine(result);
         }
 
         private static string[] GetLines()
         {
             return System.IO.File.ReadAllLines("input.txt");
-            /*
-            remember to add the command
-            
-               copy "$(ProjectDir)\input.txt" "$(TargetDir)\input.txt"
-
-            to post build events in project properties
-            */
         }
     }
 }
