ff1e1223d8176178d49737f61dfffdfe17b580fb
Kamil <kamilciag123@gmail.com>
2022-12-10 18:56:28 +0100

    Finished day10 puzzle1

--- Day10/D10Solution.cs
+++ Day10/D10Solution.cs
@@ -13,6 +13,33 @@ namespace Day10
         static void SolvePuzzle1()
         {
             string[] lines = GetLines();
+
+            int cycle = 0;
+            int register = 1;
+            int sum = 0;
+
+            foreach(string line in lines)
+            {
+                string[] split = line.Split(' ');
+
+                cycle++; //noop takes 1 cycle, and addx increases the value after 2 cycles, either way we need one cycle to pass without changing any value
+                
+                if ((cycle + 20) % 40 == 0 && cycle <= 220)
+                {
+                    sum += cycle * register;
+                }
+
+                if(split[0].Equals("addx"))
+                {
+                    cycle++;
+                    if ((cycle + 20) % 40 == 0 && cycle <= 220)
+                    {
+                        sum += cycle * register;
+                    }
+                    register += Int32.Parse(split[1]);
+                }
+            }
+            Console.WriteLine(sum);
         }
 
         static void SolvePuzzle2()
@@ -23,13 +50,6 @@ namespace Day10
         private static string[] GetLines()
         {
             return System.IO.File.ReadAllLines("input.txt");
-            /*
-            remember to add the command
-            
-               copy "$(ProjectDir)\input.txt" "$(TargetDir)\input.txt"
-
-            to post build events in project properties
-            */
         }
     }
 }
