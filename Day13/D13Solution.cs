d95e89ac95c5319f869decb871d956ae00160788
Kamil <kamilciag123@gmail.com>
2022-12-13 10:27:47 +0100

    Setup project for day 13

---
+++ Day13/D13Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day13
+{
+    class D13Solution
+    {
+        static void Main(string[] args)
+        {
+            SolvePuzzle1();
+            SolvePuzzle2();
+        }
+
+        static void SolvePuzzle1()
+        {
+            string[] lines = GetLines();
+        }
+
+        static void SolvePuzzle2()
+        {
+            string[] lines = GetLines();
+        }
+
+        private static string[] GetLines()
+        {
+            return System.IO.File.ReadAllLines("input.txt");
+        }
+    }
+}
