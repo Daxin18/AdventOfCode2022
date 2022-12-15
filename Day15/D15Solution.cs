f9c77ec331e682c4f3b6b8d6c6ae20001d61b9e6
Kamil <kamilciag123@gmail.com>
2022-12-15 13:29:10 +0100

    Setup project for day 15

---
+++ Day15/D15Solution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day15
+{
+    class D15Solution
+    {
+        static void Main(string[] args)
+        {
+            SolvePuzzle1();
+            SolvePuzzle2();
+        }
+
+        static void SolvePuzzle1()
+        {
+            string[] lines = GetLines();
+        }
+
+        static void SolvePuzzle2()
+        {
+            string[] lines = GetLines();
+        }
+
+        private static string[] GetLines()
+        {
+            return System.IO.File.ReadAllLines("input.txt");
+            /*
+            remember to add the command
+            
+               copy "$(ProjectDir)\input.txt" "$(TargetDir)\input.txt"
+
+            to post build events in project properties
+            */
+        }
+    }
+}
