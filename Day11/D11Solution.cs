742de4bd1baeaff9bb8fdbe45b4ee9720296b84e
Kamil <kamilciag123@gmail.com>
2022-12-17 16:47:32 +0100

    Setup project for day 16

--- Day11/D11Solution.cs
+++ Day11/D11Solution.cs
@@ -56,13 +56,13 @@ namespace Day11
         static void SolvePuzzle2()
         {
             string[] lines = GetLines();
-            List<Monkey> monkeys = new List<Monkey>();
+            List<NoOverflowMonkey> monkeys = new List<NoOverflowMonkey>();
             List<nuint> itemsInspected = new List<nuint>();
             int roundCount = 10000;
 
-            SetupMonkeys(lines, ref monkeys, false);
+            SetupNoOverflowMonkeys(lines, ref monkeys, false);
 
-            foreach (Monkey monkey in monkeys)
+            foreach (NoOverflowMonkey monkey in monkeys)
             {
                 itemsInspected.Add(0);
                 //Console.WriteLine(monkey.ToString());
@@ -94,6 +94,88 @@ namespace Day11
 
             Console.WriteLine(result);
         }
+
+        private static void SetupNoOverflowMonkeys(string[] lines, ref List<NoOverflowMonkey> monkeys, bool divideByThree)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int fullMonkey = 1; //first line is just monkey number (which we automatically get via list index placement
+                i++; //we omit the line with monkey number
+                string[] split;
+                List<int> monkeyItems = new List<int>();
+                Func<nuint, nuint, nuint> monkeyOperation = Addition;
+                nuint? monkeyOperationNumber = 0;
+                nuint monkeyTest = 0;
+                int monkeyTrue = 0;
+                int monkeyFalse = 0;
+                while (fullMonkey < 6) //monkey information consists of 6 lines
+                {
+                    switch (fullMonkey)
+                    {
+                        case 1: //monkey items
+                            split = lines[i].Split(' ', ',');
+                            nuint tmp;
+                            foreach (string s in split)
+                            {
+                                if (nuint.TryParse(s, out tmp))
+                                {
+                                    monkeyItems.Add((int)tmp);
+                                }
+                            }
+                            break;
+                        case 2: //monkey operation
+                            split = lines[i].Split(' ');
+                            for (int j = 0; j < split.Length; j++)
+                            {
+                                if (split[j] == "*")
+                                {
+                                    monkeyOperation = Multiplication;
+                                    if (!split[j + 1].Equals("old"))
+                                    {
+                                        monkeyOperationNumber = nuint.Parse(split[++j]);
+                                    }
+                                    else
+                                    {
+                                        monkeyOperationNumber = null;
+                                    }
+                                    break;
+                                }
+                                else if (split[j] == "+")
+                                {
+                                    monkeyOperation = Addition;
+                                    if (!split[j + 1].Equals("old"))
+                                    {
+                                        monkeyOperationNumber = nuint.Parse(split[++j]);
+                                    }
+                                    else
+                                    {
+                                        monkeyOperationNumber = null;
+                                    }
+                                    break;
+                                }
+                            }
+                            break;
+                        case 3: //monkey test
+                            split = lines[i].Split(' ');
+                            monkeyTest = nuint.Parse(split[split.Length - 1]);
+                            break;
+                        case 4: //monkey test is true
+                            split = lines[i].Split(' ');
+                            monkeyTrue = Int32.Parse(split[split.Length - 1]);
+                            break;
+                        case 5: //monkey test is false
+                            split = lines[i].Split(' ');
+                            monkeyFalse = Int32.Parse(split[split.Length - 1]);
+                            break;
+                    }
+                    fullMonkey++;
+                    i++; //we go to the next line
+                }
+
+                monkeys.Add(new NoOverflowMonkey(monkeyItems, monkeyOperation, (int?)monkeyOperationNumber, (int)monkeyTest, monkeyTrue, monkeyFalse, divideByThree));
+            }
+        }
+
 
         private static void SetupMonkeys(string[] lines, ref List<Monkey> monkeys, bool divideByThree)
         {
---
+++ Day11/NoOverflowMonkey.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    class NoOverflowMonkey
+    {
+        public class Item
+        {
+            public int number;
+            public HashSet<int> primes;
+            private static readonly int[] PRIME_LIST = { 2, 3, 5, 7, 11, 13, 17, 19};
+
+            public Item(int number)
+            {
+                this.number = (int)number;
+                primes = new HashSet<int>();
+                UpdateItem();
+            }
+
+            public void UpdateItem()
+            {
+                bool furtherDivisible = true;
+                while (furtherDivisible)
+                {
+                    furtherDivisible = false;
+                    foreach (int prime in PRIME_LIST)
+                    {
+                        if (number % prime == 0)
+                        {
+                            furtherDivisible = true;
+                            number /= prime;
+                            primes.Add(prime);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            public void Add(int i)
+            {
+                number += i;
+                UpdateItem();
+            }
+            
+            public void Multiply(int i)
+            {
+                number *= i;
+                UpdateItem();
+            }
+
+            public bool isDivisibleBy(int divisor)
+            {
+                return (primes.Contains(divisor) || number % divisor == 0);
+            }
+        }
+
+        public List<Item> items;
+        public Func<nuint, nuint, nuint> operation;
+        private int? operationNumber;
+        public int testNumber;
+        public int throwToIfTrue;
+        public int throwToIfFalse;
+        public bool divideByThree;
+
+        public NoOverflowMonkey(List<int> items, Func<nuint, nuint, nuint> operation, int? operationNumber, int testNumber, int throwToIfTrue, int throwToIfFalse, bool divideByThree)
+        {
+            List<Item> itemsList = new List<Item>();
+
+            foreach(int item in items)
+            {
+                itemsList.Add(new Item(item));
+            }
+
+            this.items = itemsList;
+            this.operation = operation;
+            this.operationNumber = operationNumber;
+            this.testNumber = testNumber;
+            this.throwToIfTrue = throwToIfTrue;
+            this.throwToIfFalse = throwToIfFalse;
+            this.divideByThree = divideByThree;
+        }
+
+        public (Item, int) InspectAndThrowItem()
+        {
+            (Item, int) result = (0, 0);
+            Item item = items[items.Count - 1]; //just to make it a bit faster to later renumber items when removing
+            int number = item.number;
+
+            checked
+            {
+                if (operationNumber is null)
+                {
+                    number = (int)operation((nuint)number, (nuint)number); //worry level increased
+                }
+                else
+                {
+                    number = (int)operation((nuint)number, (nuint)operationNumber); //worry level increased
+                }
+            }
+
+            if (divideByThree)
+            {
+                number = number / 3; //monkey gets bored
+            }
+
+            item.number = number;
+            item.UpdateItem();
+            
+            if(item.isDivisibleBy(testNumber))
+            {
+                result = (item, throwToIfTrue);
+            }
+            else
+            {
+                result = (item, throwToIfFalse);
+            }
+
+            items.RemoveAt(items.Count - 1);
+
+            return result;
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void AddItem(int item)
+        {
+            items.Add(new Item(item));
+        }
+
+        override public string ToString()
+        {
+            string itemss = items[0].ToString();
+
+            for (int i = 1; i < items.Count; i++)
+            {
+                itemss += $", {items[i]}";
+            }
+
+            return $"Items: {itemss}\nOperationNumber: {operationNumber}\nTest: divisible by {testNumber}\n\tIf True: throw to {throwToIfTrue}\n\tIf False: throw to {throwToIfFalse}";
+        }
+    }
+}
